38013bbabe0cb27faba658f5f08f3e3392284e57
Don <don.dongjun.yu@gmail.com>
2020-09-17 17:43:24 -0400

    install axios, proxy, concurrently

--- server/index.js
+++ server/index.js
@@ -26,6 +26,10 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!~~ "));
 
+app.get("/api/hello", (req, res) => {
+  res.send("Hello");
+});
+
 app.get("/api/hello", (req, res) => res.send("Hello World!~~ "));
 
 app.post("/api/users/register", (req, res) => {
