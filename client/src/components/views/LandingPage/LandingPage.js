e74df0fb9929ff8bbff629cca2329447dd8a0c52
Don <don.dongjun.yu@gmail.com>
2020-09-17 19:33:30 -0400

    set Sign-in page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,26 @@
-import React, {useEffect} from 'react';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function LandingPage() {
   useEffect(() => {
-      axios.get('/api/hello')
-      .then(response => {console.log(response)})
-  }, [])
+    axios.get("/api/hello").then((response) => {
+      console.log(response);
+    });
+  }, []);
 
   return (
-    <div>
-      LandingPage 랜딩페이지
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작페이지</h2>
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
